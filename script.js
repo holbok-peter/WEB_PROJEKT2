4008c8e51eb0316994ae233a7afc204f9b79c836
holbok-peter <holbok.peter@students.jedlik.eu>
2024-02-12 13:51:27 +0100

    szuiiiiii

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
 let karakter = document.querySelector(".karakter");
 let ellenseg = document.querySelector(".ellenseg");
 let tudos = document.querySelector(".tudos");
+let karakterPosition = karakter.style.bottom || 400;
+let repülésiSebesség = 0;
+let repül = false;
+
+
+document.addEventListener("keypress", ()=>{
+    repülésiSebesség += 30;
+    karakter.style.bottom = (karakterPosition + repülésiSebesség) + "px";
+    repül = true;
+});
+
+
+setInterval(()=>{
+    if(repülésiSebesség > 0){
+        repülésiSebesség -= 1;
+        karakter.style.bottom = (karakterPosition + repülésiSebesség) + "px";
+    }
+} , 1)
