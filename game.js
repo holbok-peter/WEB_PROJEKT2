cbe7633cb392147c66c3ff03c6f3677ebef4141b
anda-kristof <98750771+anda-kristof@users.noreply.github.com>
2024-02-25 14:19:07 +0000

    game coin collecting

--- game.js
+++ game.js
@@ -1,8 +1,21 @@
-import {Mozgas} from "./Movement.js"
-
+import { Mozgas, karakterPosition} from "./Movement.js";
+import { coinGeneralas, coinCheck, coinMove } from "./coin.js";
+let pontszam = document.querySelector(".goldok");
+let jatekVege = false;
 let score = 0;
 let gold = 0;
+let life = 3
 
-setInterval(()=>{
-Mozgas()
-},20)
+setInterval(() => {
+    let karakterr = document.querySelector(".karakter").getBoundingClientRect();
+   gold=  coinCheck(karakterPosition, gold);
+  Mozgas();
+  coinMove();
+ pontszam.innerHTML = gold;
+}, 20);
+
+setInterval(() => {
+  coinGeneralas();
+}, 2000);
+
+
