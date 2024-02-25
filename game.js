8bc3c04cf0fef6a024bc5771f21d496ebcb3b988
holbok-peter <holbok.peter@students.jedlik.eu>
2024-02-25 19:42:13 +0100

    a

--- game.js
+++ game.js
@@ -8,21 +8,11 @@ let life = 3;
 
 Mozgas();
 setInterval(() => {
-<<<<<<< HEAD
-  let karakterr = document.querySelector(".karakter").getBoundingClientRect();
-  gold = coinCheck(karakterPosition, gold);
-  Mozgas();
-  MoveScientist();
-  coinMove();
-  pontszam.innerHTML = gold;
-}, 5);
-=======
     
    gold=  coinCheck(karakterPosition, gold);
   coinMove();
  pontszam.innerHTML = gold;
 }, 5);
->>>>>>> bee583aa69a049e12e383ee00e838984c7ae50f9
 
 setInterval(() => {
   coinGeneralas();
